7ae737db59cb08c01e6a338c082345df3517638a
Sebastiiii <sebastiii@team-mediaportal.com>
2026-03-23 22:22:48 +0100

    avdvplus R9

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -414,6 +414,22 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
 
     bool m_bypassDVModeSwitch = false;
 
+    void SetAlgoForResetSub(int num_resets);
+    int GetAlgoForResetSub() const;
+    int m_algoForResetSub;
+
+    void SetLastResetTimeSub(double reset_time);
+    double GetLastResetTimeSub() const;
+    double m_lastResetTimeSub;
+
+    void SetResetSyncSub(bool reset_sync);
+    bool GetResetSyncSub() const;
+    bool m_resetSyncSub;
+
+    void SetResetSeekSub(bool reset_seek);
+    bool GetResetSeekSub() const;
+    bool m_resetSeekSub;
+
     void SetLimitCD(bool limit_cd);
     bool GetLimitCD() const;
     bool m_limitCD;
